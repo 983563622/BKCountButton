5d7e75ec36547ddfaf0543237bcfec86ba1a8ede
king <983563622@qq.com>
2015-09-18 16:37:06 +0800

    Initial Commit

---
+++ BKCountButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BKCountButton
+//
+//  Created by apple on 15/9/18.
+//  Copyright (c) 2015年 king. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
